d18a7947ca8bc91f7e826da08b0895cb78327133
PratikMahobiyaAppiness <pratik@appinessworld.com>
2022-03-08 09:19:51 +0530

    UPDATE:-

--- cart/views.py
+++ cart/views.py
@@ -86,7 +86,7 @@ class OrderView(APIView):
 								'address':request.POST.get('address',''),'amount':request.POST.get('amount',''),
 								'no_of_items':request.POST.get('no_of_items','')
 							}
-		Ord_Serializer = serializers.OrderSerializer(data=ord_data)
+		Ord_Serializer = serializers.OrderSerializer(data=ord_data, partial=True)
 		if not Ord_Serializer.is_valid():
 			return JsonResponse({'status':403, 'errors': Ord_Serializer.errors})
 		Ord_Serializer.save()
@@ -113,5 +113,53 @@ class OrderView(APIView):
 								'product_list':res_product,
 								'order_date': Ord_Serializer.data['order_date']
 							}
+		order_currency = 'INR'
+		callback_url = 'http://'+ str(get_current_site(request))+"/handlerequest/"
+		notes = {'order-type': "basic order from the website", 'key':'value'}
+		razorpay_order = razorpay_client.order.create(dict(amount=int(request.POST.get('amount',0))*100, currency=order_currency, notes = notes, receipt=Ord_Serializer.data['bill_no'], payment_capture='0'))
+		print(razorpay_order['id'])
+		order = models.Order.objects.get(bill_no = Ord_Serializer.data['bill_no'])
+		order.razorpay_order_id = razorpay_order['id']
+		order.save()
+
 		models.UserCart.objects.filter(user_id = user_id).delete()
-		return JsonResponse({'status':200, 'data': res_data, 'message':'Order is Placed.'})
+		return JsonResponse({'status':200, 'data': res_data,'message':'Order is Placed.','order':order, 'order_id': razorpay_order['id'], 'orderId':order.order_id, 'final_price':request.POST.get('amount',0), 'razorpay_merchant_id':settings.razorpay_id, 'callback_url':callback_url})
+
+@csrf_exempt
+def handlerequest(request):
+	if request.method == "POST":
+		try:
+				payment_id = request.POST.get('razorpay_payment_id', '')
+				order_id = request.POST.get('razorpay_order_id','')
+				signature = request.POST.get('razorpay_signature','')
+				params_dict = { 
+				'razorpay_order_id': order_id, 
+				'razorpay_payment_id': payment_id,
+				'razorpay_signature': signature
+				}
+				try:
+						order_db = models.Order.objects.get(razorpay_order_id=order_id)
+				except:
+						return JsonResponse({'status':505})
+				order_db.razorpay_payment_id = payment_id
+				order_db.razorpay_signature = signature
+				order_db.save()
+				result = razorpay_client.utility.verify_payment_signature(params_dict)
+				if result==None:
+					amount = order_db.total_amount * 100   #we have to pass in paisa
+					try:
+						razorpay_client.payment.capture(payment_id, amount)
+						order_db.payment_status = 1
+						order_ = order_db.save()
+						order_serializer = serializers.OrderSerializer(order_)
+						return JsonResponse({'status':200, 'data': order_serializer.data,'message':'Order is Placed.'})
+					except:
+						order_db.payment_status = 2
+						order_db.save()
+						return JsonResponse({'status':505, 'data': order_serializer.data,'message':'Order is Placed.'})
+				else:
+					order_db.payment_status = 2
+					order_db.save()
+					return JsonResponse({'status':505, 'data': order_serializer.data,'message':'Order is Placed.'})
+		except:
+				return JsonResponse({'status':505})
