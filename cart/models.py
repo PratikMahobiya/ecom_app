550005bfdccd2cc2ed52ea06359b70aedb5bb8e1
PratikMahobiyaAppiness <pratik@appinessworld.com>
2022-03-08 09:25:18 +0530

    UPDATE:-

--- cart/models.py
+++ cart/models.py
@@ -12,15 +12,33 @@ class UserCart(models.Model):
         return str(self.id)
 
 class Order(models.Model):
+    status_choices = (
+        (1, 'Not Packed'),
+        (2, 'Ready For Shipment'),
+        (3, 'Shipped'),
+        (4, 'Delivered')
+    )
+    payment_status_choices = (
+        (1, 'SUCCESS'),
+        (2, 'FAILURE' ),
+        (3, 'PENDING'),
+    )
     user_id 				= models.ForeignKey(User, on_delete=models.CASCADE,verbose_name='User Id')
     bill_no                 = models.AutoField(primary_key=True,verbose_name='Bill No.')
+    status                  = models.IntegerField(choices = status_choices, default=1)
+    amount            		= models.FloatField(verbose_name='Order_amount')
+    payment_status          = models.IntegerField(choices = payment_status_choices, default=3)
     first_name              = models.CharField(max_length=100,verbose_name='First Name')
     last_name               = models.CharField(max_length=100,verbose_name='Last Name')
     mobile   				= models.BigIntegerField(verbose_name='Contact Number')
     address     			= models.TextField(verbose_name='Address')
-    amount            		= models.FloatField(verbose_name='Order_amount')
     no_of_items        		= models.PositiveBigIntegerField(verbose_name='Number Of Items')
     order_date              = models.DateTimeField(auto_now_add=True)
+
+    # related to razorpay
+    razorpay_order_id       = models.CharField(max_length=500, null=True, blank=True)
+    razorpay_payment_id     = models.CharField(max_length=500, null=True, blank=True)
+    razorpay_signature      = models.CharField(max_length=500, null=True, blank=True)
     def __str__(self):
         return str(self.bill_no)
 
